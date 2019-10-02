3b7d4b2d7de7915a106352b4f5eed5325a55cd70
Peter Banjo <peterbanjo@gmail.com>
2019-10-02 11:59:28 +0100

    install react-native-maps. show broken getMapBoundaries

--- App.tsx
+++ App.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
+  const mapRef = useRef<MapView>(null);
+
+  const handleRegionChange = async () => {
+    const map = mapRef.current;
+    await map.getMapBoundaries();
+    console.log(typeof map.getMapBoundaries);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <MapView
+      ref={mapRef}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+      onRegionChangeComplete={handleRegionChange}
+      style={styles.container}
+    />
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
